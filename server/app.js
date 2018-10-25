931d40dc1bd26a7434ae810a13eda06bc3805914
ozaki <fs65ugumori@yahoo.co.jp>
2018-10-26 07:43:06 +0900

    add cors settings to express

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000, () => {
